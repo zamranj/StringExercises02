d3c492926e6eed70ed0523f7c05f8ee6c1b57b67
zeejay21 <>
2021-06-05 19:09:51 +0000

    Excercise part 1

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 let num = 1001;
-
+let num2 = 123.45;
 //Returns 'undefined'.
 console.log(num.length);
 
 //Use type conversion to print the length (number of digits) of an integer.
-
-
+console.log(String(num).length);
 
 
 //Follow up: Print the number of digits in a DECIMAL value (e.g. num = 123.45 has 5 digits but a length of 6).
 
-
+if(String(num2).includes(".")){
+  console.log(String(num2).length-1);
+}else{
+  console.log(String(num2).length);
+}
 
 
 //Experiment! What if num could be EITHER an integer or a decimal?  Add an if/else statement so your code can handle both cases.
